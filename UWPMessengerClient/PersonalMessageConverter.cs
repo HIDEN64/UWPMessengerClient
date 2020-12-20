179424bd529e687f507c71c8f6099cc344f6670f
campos02 <vinibohm@hotmail.com>
2020-12-19 22:14:41 -0300

    personal message now shows text saying to click if add it if empty

---
+++ UWPMessengerClient/PersonalMessageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPMessengerClient
+{
+    class PersonalMessageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string psm = (string)value;
+            if (psm == "" || psm == null)
+            {
+                psm = "Click here to add your personal message";
+            }
+            return psm;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
